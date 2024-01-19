b3c8fd7bfba32d17dc0e49cbcd3ec8091f980e89
riverosmejia <113913659+riverosmejia@users.noreply.github.com>
2024-01-18 23:29:43 -0500

    Add files via upload

--- proyect_pp/tu_programa_windows.bat
+++ proyect_pp/tu_programa_windows.bat
@@ -1,2 +1,14 @@
-E:\home\miguel_riveros\Documentos\proyect1\proyect_pp\tu_programa
-pause
+@echo off
+setlocal enabledelayedexpansion
+
+rem Encuentra la ruta del directorio actual del script
+set "SCRIPT_DIR=%~dp0"
+
+rem Puedes definir una variable para el archivo ejecutable
+set "PROGRAM_FILE=tu_programa.exe"
+
+rem Ejecuta el programa desde el directorio del script
+cd /d !SCRIPT_DIR!
+!SCRIPT_DIR!!PROGRAM_FILE!
+
+endlocal
